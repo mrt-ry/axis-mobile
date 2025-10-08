a67ba9f864a2ce5d6a95b3836d130b2f83a17f37
村田怜哉 <murataryouya@muratareinarinoMacBook-Pro.local>
2025-10-08 22:04:36 +0900

    定数ファイルを作成

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color baseColor = Color(0xffffffff);
+  static const Color primaryColor = Color(0xff75EBD0);
+  static const Color secondaryColor = Color(0xffF58523);
+  static const Color textColor = Color(0x33333333);
+  static const Color subTextColor = Color(0xE4E4E4E4);
+}
